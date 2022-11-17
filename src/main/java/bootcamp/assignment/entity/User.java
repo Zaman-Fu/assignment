47a3546e8bf43212aa8c4920d05990b589b5beb9
Philip Zamanis <philip.zamanis@solera.com>
2022-11-17 10:17:10 +0100

    added missing imports

--- src/main/java/bootcamp/assignment/entity/User.java
+++ src/main/java/bootcamp/assignment/entity/User.java
@@ -4,6 +4,7 @@ package bootcamp.assignment.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
 
 
 @Entity
