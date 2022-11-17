338ec4bdff871cf7a6bbc1ef5ed66374ef1c9266
Philip Zamanis <philip.zamanis@solera.com>
2022-11-17 10:21:46 +0100

    added Id import

--- src/main/java/bootcamp/assignment/entity/User.java
+++ src/main/java/bootcamp/assignment/entity/User.java
@@ -1,6 +1,7 @@
 package bootcamp.assignment.entity;
 
 
+import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
