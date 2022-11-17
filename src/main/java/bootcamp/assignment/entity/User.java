4c0fe5554ea18d4a66d07228c0a01b563732ad2d
Philip Zamanis <philip.zamanis@solera.com>
2022-11-17 10:16:14 +0100

    added User entity with the appropriate table name

--- src/main/java/bootcamp/assignment/entity/User.java
+++ src/main/java/bootcamp/assignment/entity/User.java
@@ -1,5 +1,88 @@
 package bootcamp.assignment.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+
+@Entity
+@Table(name = "dbo.palaczvargazamanis")
 public class User {
 
+	@Id
+	@GeneratedValue
+	Long Id;
+	
+	@Column (name = "firstName")
+	String firstName;
+	
+	@Column (name = "lastName")
+	String lastName;
+	
+	@Column (name = "phoneNumber")
+	String phoneNumber;
+	
+	@Column (name = "email")
+	String email;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public User() {}
+	public User(Long id, String firstName, String lastName, String phoneNumber, String email) {
+		super();
+		Id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber
+				+ ", email=" + email + "]";
+	}
+	
+	
+	
+	
 }
